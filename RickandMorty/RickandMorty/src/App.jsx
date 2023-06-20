d313b35985fa352b88180e17a7d2a5e022d55b2c
Revoo991 <rafael.revo1218@gmail.com>
2023-06-19 20:18:19 -0600

    +

--- RickandMorty/RickandMorty/src/App.jsx
+++ RickandMorty/RickandMorty/src/App.jsx
@@ -1,27 +1,24 @@
 /* eslint-disable react/jsx-no-undef */
-// eslint-disable-next-line no-unused-vars
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './assets/css/style.css';
-import Comodin from './components/Comodin';
-import Header from './components/Header';
-import CardPersonajes from './pages/CardPersonajes';
-import Home from './pages/Home';
-import ListaPersonajes from './pages/ListaPersonajes';
 
-export default function App() {
+// eslint-disable-next-line no-unused-vars
+
+import './assets/css/style.css'
+import Header from './components/Header';
+import ListaPersonajes from './components/ListaPersonajes';
+
+
+
+function App() {
+  
   return (
     <>
-      <BrowserRouter>
-      <Header/>
-
-        <Routes>
-          <Route exact path='/' element={<Home />}/>
-          <Route path='comodin' element={<Comodin />}/>
-          <Route path='lista-personajes' element={<ListaPersonajes />}/>
-          <Route path='card-personajes' element={<CardPersonajes />}/>
-          {/* <Route path='*' element={<NotFound />}/> */}
-        </Routes>
-      </BrowserRouter>
+    <Header/>
+      <div className='container'>
+        <ListaPersonajes/>
+        
+      </div>
     </>
   )
 }
+
+export default App
