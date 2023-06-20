ec6758ef46f229d810dda0227fa5ff9212267d48
Revoo991 <rafael.revo1218@gmail.com>
2023-06-20 15:56:06 -0600

    cambio de tabal por card

--- RickandMorty/RickandMorty/src/components/CardPersonajes.jsx
+++ RickandMorty/RickandMorty/src/components/CardPersonajes.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
@@ -33,47 +34,44 @@ export default function CardPersonajes({
   };
 
   return (
-    <div>
-      <div className='contenedor-img'>
-        <h2>{name}</h2>
-        <img src={image} alt={name} />
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-lg-4 col-md-6">
+          <div className="card mb-4 h-100 custom-card" style={{ width: '23rem' }}>
+            <img src={image} className="card-img-top card-text" alt={name} />
+            <div className="card-body card-text">
+              <h5 className="card-title titulo">{name}</h5>
+              <div className="card-content">
+                <p className="card-text">
+                  <strong>Id:</strong> {id}
+                </p>
+                <p className="card-text ">
+                  <strong>Origin:</strong> {origin}
+                </p>
+                <p className="card-text">
+                  <strong>Specie:</strong> {specie}
+                </p>
+                <p className="card-text">
+                  <strong>Status:</strong>{' '}
+                  <span style={{ color: getStatusColor() }}>{status}</span>
+                </p>
+                <p className="card-text">
+                  <strong>Gender:</strong> {gender}
+                </p>
+                <p className="card-text locacionn">
+                  <strong>Location:</strong> {location}
+                </p>
+                <p className="card-text">
+                  <strong>Episodes:</strong> {episodes}
+                </p>
+                <p className="card-text">
+                  <strong>Type:</strong> {type}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <table className='table'>
-        <tbody>
-          <tr>
-            <td className='title'>Id</td>
-            <td>{id}</td>
-          </tr>
-          <tr>
-            <td className='title'>origin</td>
-            <td>{origin}</td>
-          </tr>
-          <tr>
-            <td className='title'>specie</td>
-            <td>{specie}</td>
-          </tr>
-          <tr>
-            <td className='title'>status</td>
-            <td style={{ color: getStatusColor() }}>{status}</td>
-          </tr>
-          <tr>
-            <td className='title'>gender</td>
-            <td>{gender}</td>
-          </tr>
-          <tr>
-            <td className='title'>location</td>
-            <td>{location}</td>
-          </tr>
-          <tr>
-            <td className='title'>Episodes</td>
-            <td>{episodes}</td>
-          </tr>
-          <tr>
-            <td className='title'> Type</td>
-            <td>{type}</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 }
