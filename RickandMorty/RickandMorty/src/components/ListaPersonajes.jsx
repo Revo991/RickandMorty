5031dd25aede9eb36e73878f82ea2052e98f796e
Revoo991 <rafael.revo1218@gmail.com>
2023-06-22 21:34:29 -0600

    arreglo en el codigo

--- RickandMorty/RickandMorty/src/components/ListaPersonajes.jsx
+++ RickandMorty/RickandMorty/src/components/ListaPersonajes.jsx
@@ -46,12 +46,16 @@ export default function ListaPersonajes() {
     const filteredPersonajes = personajesOriginales.filter((personaje) => {
       const nombreMatch =
         personaje.name.toLowerCase().includes(filtro.toLowerCase()) ||
-        String(personaje.id) === filtro ||
-        String(personaje.species) === filtro ||
-        String(personaje.location.name) === filtro;
+        personaje.id.toString() === filtro ||
+        personaje.species.toString() === filtro ||
+        personaje.location.name.toLowerCase() === filtro.toLowerCase();
 
       const estadoMatch =
-        estadoFiltro === '' ? true : estadoFiltro === 'Unknown' ? personaje.status === 'unknown' : personaje.status === estadoFiltro;
+        estadoFiltro === ''
+          ? true
+          : estadoFiltro === 'Unknown'
+          ? personaje.status === 'unknown'
+          : personaje.status === estadoFiltro;
 
       return nombreMatch && estadoMatch;
     });
@@ -70,6 +74,9 @@ export default function ListaPersonajes() {
   };
 
   const mostrarSinResultados = personajes.length === 0 && filtro !== '';
+
+ 
+
 
   return (
     <div className="fondo">
