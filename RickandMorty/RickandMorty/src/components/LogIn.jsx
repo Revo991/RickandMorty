ccffb214b2ce322b87c4e43e836798b4f858ccab
alissonserpas <127900340+alissonserpas@users.noreply.github.com>
2023-06-24 18:00:45 -0600

    diseno-login

--- RickandMorty/RickandMorty/src/components/LogIn.jsx
+++ RickandMorty/RickandMorty/src/components/LogIn.jsx
@@ -11,6 +11,16 @@ export default function LogIn() {
 
 const {loginWithRedirect} = useAuth0();
   return (
+    <main className='fondo-2 p-5' >
+      <div className="row">
+        <div className='col-6'>
+          <img src={mortygif} alt="portal"/>
+        </div>
+        <div className='col-5 position-absolute top-100 start-50'>
+        <h2 className="text-white">BIENVENIDOS</h2>
         <button className='btn btn-primary' onClick={ () => loginWithRedirect()}>Iniciar Sesion</button>
+        </div>
+      </div>
+    </main>
   )
 }
